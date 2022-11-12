043f2fc86f3def97686239f3c6dd43c64dc9c519
Darren Grafton <darrenmgrafton@gmail.com>
2022-11-12 09:41:05 -0600

    1-1 deployed

---
+++ EasyAzure.Web/Controllers/AppServiceController.cs
@@ -0,0 +1,32 @@
+﻿using EasyAzure.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EasyAzure.Controllers
+{
+    public class AppServiceController : Controller
+    {
+        private readonly ILogger<AppServiceController> _logger;
+
+        public AppServiceController(ILogger<AppServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
